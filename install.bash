4c2a36b18bf87321f076c9f6c3f170a43701d28c
Sly_tom_cat <slytomcat@mail.ru>
2018-01-18 18:28:35 +0300

    temporary fix for https://github.com/visualfc/goqt/issues/52

--- install.bash
+++ install.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#mv yd-qgo /usr/bin/
+mv yd-qgo /usr/bin/
 mkdir -p /usr/share/yd-qgo/icons/dark
 mkdir -p /usr/share/yd-qgo/icons/light
 cp icons/yd* /usr/share/yd-qgo/icons
 cp icons/dark/* /usr/share/yd-qgo/icons/dark/
 cp icons/light/* /usr/share/yd-qgo/icons/light/
-cp README.md /usr/share/yd-qgo/
+#cp README.md /usr/share/yd-qgo/
 
