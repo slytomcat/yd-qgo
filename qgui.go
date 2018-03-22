40874c884055018c415aa11ff476f620e45f4b41
Sly_tom_cat <slytomcat@mail.ru>
2018-03-22 19:30:25 +0300

    appInit in tools

--- qgui.go
+++ qgui.go
@@ -44,9 +44,7 @@ Copyleft 2017-2018 Sly_tom_cat (slytomcat@mail.ru)
 `
 
 var (
-	// AppConfigFile stores the application configuration file path
-	AppConfigFile string
-	// Msg is the Localozation printer
+	// Msg is the Localization printer
 	Msg *message.Printer
 
 	iconBusy  [5]*ui.QIcon
@@ -61,54 +59,10 @@ func main() {
 
 func onStart() {
 	C.savesigchld() // temporary fix for https://github.com/visualfc/goqt/issues/52
-	var debug bool
-	flag.BoolVar(&debug, "debug", false, "Allow debugging messages to be sent to stderr")
-	flag.StringVar(&AppConfigFile, "config", "~/.config/yd-qgo/default.cfg", "Path to the indicator configuration file")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage:\n\n\t\tyd-qgo [-debug] [-config=<Path to indicator config>]\n\n")
-		flag.PrintDefaults()
-	}
-	flag.Parse()
-	// Initialize logging facility
-	llog.SetOutput(os.Stderr)
-	llog.SetPrefix("")
-	llog.SetFlags(log.Lshortfile | log.Lmicroseconds)
-	if debug {
-		llog.SetLevel(llog.DEBUG)
-		llog.Info("Debugging enabled")
-	} else {
-		llog.SetLevel(-1)
-	}
+	// Initialize application and receive the application configuration
+	AppCfg := tools.AppInit("yd-qgo")
 	// Initialize translations
 	Msg = message.NewPrinter(message.MatchLanguage("ru"))
-	// Prepare the application configuration
-	// Make default app configuration values
-	AppCfg := map[string]interface{}{
-		"Conf":          tools.ExpandHome("~/.config/yandex-disk/config.cfg"), // path to daemon config file
-		"Theme":         "dark",                                               // icons theme name
-		"Notifications": true,                                                 // display desktop notification
-		"StartDaemon":   true,                                                 // start daemon on app start
-		"StopDaemon":    false,                                                // stop daemon on app closure
-	}
-	// Check that app configuration file path exists
-	AppConfigHome := tools.ExpandHome("~/.config/yd-qgo")
-	if tools.NotExists(AppConfigHome) {
-		err := os.MkdirAll(AppConfigHome, 0766)
-		if err != nil {
-			llog.Critical("Can't create application configuration path:", err)
-		}
-	}
-	// Path to app configuration file path always comes from command-line flag
-	AppConfigFile = tools.ExpandHome(AppConfigFile)
-	llog.Debug("Configuration:", AppConfigFile)
-	// Check that app configuration file exists
-	if tools.NotExists(AppConfigFile) {
-		//Create and save new configuration file with default values
-		confJSON.Save(AppConfigFile, AppCfg)
-	} else {
-		// Read app configuration file
-		confJSON.Load(AppConfigFile, &AppCfg)
-	}
 	// Create new ydisk interface
 	YD, err := ydisk.NewYDisk(AppCfg["Conf"].(string))
 	if err != nil {
